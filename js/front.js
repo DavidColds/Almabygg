9be088b1a3b67044e6e6d89e91d241fdfce04290
David Drughi <drughidavid6@gmail.com>
2020-09-02 16:41:13 +0200

    first commit

---
+++ js/front.js
@@ -0,0 +1,41 @@
+$(function () {
+
+
+    // ================================================
+    //  HERO SLIDER
+    // ================================================
+    $('.hero-slider').owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        loop: true
+    });
+
+    // ================================================
+    //  TESTIMONIALS SLIDER
+    // ================================================
+    $('.testimonials-slider').owlCarousel({
+        items: 1,
+        margin: 30,
+        dots: true,
+        responsive: {
+            991: {
+                items: 2
+            }
+        }
+    });
+});
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('animation');
+    $("#gallery").fadeTo(300, 1);
+    }, 300);
+    });
+    });
